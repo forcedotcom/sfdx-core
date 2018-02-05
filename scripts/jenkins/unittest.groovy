fd9831503670ec4019459b24111e28fdacdf9ab8
Tim Noonan <tnoonan@salesforce.com>
2018-02-05 12:51:40 -0700

    Proxy config

--- scripts/jenkins/unittest.groovy
+++ scripts/jenkins/unittest.groovy
@@ -2,7 +2,11 @@ library identifier: 'salesforcedx-library'
 
 
 withEnv([
-	'USE_GENERIC_UNIX_KEYCHAIN=true'
+	'USE_GENERIC_UNIX_KEYCHAIN=true',
+	'http_proxy=http://public0-proxy1-0-prd.data.sfdc.net:8080',
+	'https_proxy=http://public0-proxy1-0-prd.data.sfdc.net:8080',
+	'HTTP_PROXY=http://public0-proxy1-0-prd.data.sfdc.net:8080',
+	'HTTPS_PROXY=http://public0-proxy1-0-prd.data.sfdc.net:8080'
 ]) {
     runUnitTestsWithCoverage(currentBuild)
 }
