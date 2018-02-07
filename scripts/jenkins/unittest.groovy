b8b7cb3908ed42963c8e8c8d0baa0c247d590e23
Tim Noonan <tnoonan@salesforce.com>
2018-02-07 15:26:35 -0700

    Wrong env variable name

--- scripts/jenkins/unittest.groovy
+++ scripts/jenkins/unittest.groovy
@@ -2,7 +2,7 @@ library identifier: 'salesforcedx-library'
 
 
 withEnv([
-	'USE_GENERIC_UNIX_KEYCHAIN=true',
+	'SFDX_USE_GENERIC_UNIX_KEYCHAIN=true',
 	'http_proxy=http://public0-proxy1-0-prd.data.sfdc.net:8080',
 	'https_proxy=http://public0-proxy1-0-prd.data.sfdc.net:8080',
 	'HTTP_PROXY=http://public0-proxy1-0-prd.data.sfdc.net:8080',
