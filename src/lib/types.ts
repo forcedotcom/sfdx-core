73560c0cddc19e87e9f89701fe1eca4551bb023d
Bob Bergman <rbergman@salesforce.com>
2018-03-15 11:49:10 -0600

    Fewer derps in json docs

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,27 +1,19 @@
 /**
+ * Any valid JSON value.
+ *
  * @typedef {boolean|number|string|null|JsonArray|JsonMap} AnyJson
  */
 /**
+ * Any JSON-compatible object.
+ *
  * @typedef {object} JsonMap
  */
 /**
+ * Any JSON-compatible array.
+ *
  * @typedef {Array<AnyJson>} JsonArray
  */
 
- /**
- * Any valid JSON value.
- */
 export type AnyJson = boolean | number | string | null | JsonArray | JsonMap;
-
-/**
- * Any JSON-compatible object.
- */
-export interface JsonMap {
-    [key: string]: AnyJson;
-}
-
-/**
- * Any JSON-compatible array.
- */
-export interface JsonArray extends Array<AnyJson> {
-}
+export interface JsonMap { [key: string]: AnyJson; }
+export interface JsonArray extends Array<AnyJson> { }
