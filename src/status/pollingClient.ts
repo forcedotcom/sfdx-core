c2dc2c2f73a5c54750f6919f93cf25490dbf1924
Tim Noonan <tnoonan@salesforce.com>
2018-10-05 09:51:43 -0600

    MyDomain DNS resolver. @W-5470957@ (#143)
    * MyDomain DNS resolver.
    
    * Fix merge issue.

--- src/status/pollingClient.ts
+++ src/status/pollingClient.ts
@@ -6,6 +6,8 @@
  */
 
 import { setInterval } from 'timers';
+
+import { AnyFunction } from '@salesforce/ts-types';
 import { Logger } from '../logger';
 import { SfdxError } from '../sfdxError';
 import { Time, TIME_UNIT } from '../util/time';
@@ -22,6 +24,7 @@ export interface PollingOptions<T> {
     frequency: Time;
     // Hard timeout for polling.
     timeout: Time;
+    timeoutErrorName?: string;
 }
 
 /**
@@ -108,36 +111,67 @@ export class PollingClient<T> {
      * @async
      */
     public subscribe(): Promise<T> {
+        // This promise is held open while setInterval tries to resolve or reject.
+        // If set interval can't do it then the timeout will reject.
         return new Promise((resolve, reject) => {
             // Use set interval to periodically call the polling function
-            this.interval = setInterval(async () => {
-                try {
-                    // Poll can be an async function.
-                    const sample: StatusResult<T> = await this.options.poll();
-                    if (sample.completed) {
-                        this.clearAll();
-                        resolve(sample.payload);
-                    }
-                } catch (e) {
-                    this.clearAll();
-                    reject(e);
+
+            // This try catch enables support for time{0} since setInterval only supports
+            // time {1}. In other words, we should call first then wait for the first interval.
+            this.doPoll().then((result: StatusResult<T> | undefined) => {
+                if (result && result.completed) {
+                    resolve(result.payload);
+                } else {
+                    this.interval = setInterval(
+                        PollingClient.prototype.doPoll.bind(this, resolve, reject),
+                        this.options.frequency.milliseconds);
                 }
-            }, this.options.frequency.milliseconds);
+            }).catch(() => {
+                this.interval = setInterval(
+                    PollingClient.prototype.doPoll.bind(this, resolve, reject),
+                    this.options.frequency.milliseconds);
+            });
 
             // hard polling timeout.
             this.timeout = setTimeout(() => {
+                this.logger.debug('Polling timed out');
                 this.clearAll();
                 // @todo should go in messages.
-                reject(new SfdxError('The client has timed out.', 'ClientTimeout'));
+                reject(new SfdxError('The client has timed out.',
+                    this.options.timeoutErrorName || 'PollingClientTimeout'));
             }, this.options.timeout.milliseconds);
         });
     }
 
+    private async doPoll(resolve?: AnyFunction, reject?: AnyFunction): Promise<StatusResult<T> | undefined> {
+        try {
+            // Poll can be an async function.
+            const sample: StatusResult<T> = await this.options.poll();
+            if (sample.completed) {
+                this.clearAll();
+                if (resolve) {
+                    resolve(sample.payload);
+                } else {
+                    return sample;
+                }
+            }
+        } catch (e) {
+            this.clearAll();
+            if (reject) {
+                reject(e);
+            } else {
+                throw e;
+            }
+        }
+    }
+
     private clearAll() {
         if (this.interval) {
+            this.logger.debug('Clearing the polling interval');
             clearInterval(this.interval);
         }
         if (this.timeout) {
+            this.logger.debug('Clearing the timeout interval');
             clearTimeout(this.timeout);
         }
     }
