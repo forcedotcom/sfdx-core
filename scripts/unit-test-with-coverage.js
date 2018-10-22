4659e7bf6a57bb07e1adc2e1afb6f0275f0fd4d9
Thomas <t.dvornik@salesforce.com>
2018-10-22 14:44:05 -0600

    Update license text

--- scripts/unit-test-with-coverage.js
+++ scripts/unit-test-with-coverage.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 
 const shell = require('shelljs');
 shell.set('-e');
--- test/unit/config/configGroupTest.ts
+++ test/unit/config/configGroupTest.ts
@@ -1,10 +1,9 @@
 /*
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 import { ensureJsonMap } from '@salesforce/ts-types';
 import { assert, expect } from 'chai';
 import * as sinon from 'sinon';
