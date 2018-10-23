5ccfc6f8c3de716fa5d550d936eeb8cebdc8db0a
Thomas Dvornik <amphro@gmail.com>
2018-10-22 22:47:58 -0600

    Update license text (#7)

--- src/schemaValidator.ts
+++ src/schemaValidator.ts
@@ -1,10 +1,9 @@
 /*
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 import { getJsonValuesByName } from '@salesforce/kit';
 import { AnyJson, asJsonArray, asJsonMap, Dictionary, getAsJsonArray, isJsonMap, isString, JsonMap, Optional } from '@salesforce/ts-types';
 import * as validator from 'jsen';
