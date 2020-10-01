66a450b0a0030d85c61645eb8c782289e4938e4a
Thomas <t.dvornik@salesforce.com>
2020-09-30 17:59:44 -0700

    fix: lifecycle should be a singleton across core versions

--- src/lifecycleEvents.ts
+++ src/lifecycleEvents.ts
@@ -12,6 +12,10 @@ import * as Debug from 'debug';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type callback = (data: any) => Promise<void>;
 
+declare const global: {
+  salesforceCoreLifecycle?: Lifecycle;
+};
+
 /**
  * An asynchronous event listener and emitter that follows the singleton pattern. The singleton pattern allows lifecycle
  * events to be emitted from deep within a library and still be consumed by any other library or tool. It allows other
@@ -31,7 +35,6 @@ type callback = (data: any) => Promise<void>;
  * ```
  */
 export class Lifecycle {
-  private static instance: Lifecycle;
   private debug = Debug(`sfdx:${this.constructor.name}`);
   private readonly listeners: Dictionary<callback[]>;
 
@@ -42,10 +45,17 @@ export class Lifecycle {
    * Retrieve the singleton instance of this class so that all listeners and emitters can interact from any library or tool
    */
   public static getInstance(): Lifecycle {
-    if (!this.instance) {
-      this.instance = new Lifecycle();
+    // Across a npm dependency tree, there may be a LOT of versions of @salesforce/core. We want to ensure that consumers are notified when
+    // listening on a lifecycle event that is fired by a different version of @salesforce/core. Adding the instance on the global object will
+    // ensure this. Note: There needs to be version checking up update lifecycle to the newer version if ANYTHING is ever added to this class.
+    // One way this can be done by adding a version = require(../package.json).version to the Lifecycle class, then checking that here.
+    // For example, if instance is created with @salesforce/core@2.12.2 and something is added in 2.14.0, someone who depends on version 2.14.0
+    // may get an instance that was created with 2.12.2.
+    // Nothing should EVER be removed, even across major versions.
+    if (!global.salesforceCoreLifecycle) {
+      global.salesforceCoreLifecycle = new Lifecycle();
     }
-    return this.instance;
+    return global.salesforceCoreLifecycle;
   }
 
   /**
