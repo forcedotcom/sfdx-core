9c6eb3ef9f660ba256b68e24b13e613bd406990e
Xiaoyi Chen <xchen@salesforce.com>
2021-02-02 13:53:12 -0800

    fix: update lib

--- lib/keyChain.d.ts
+++
@@ -1,7 +0,0 @@
-import { KeyChain } from './keyChainImpl';
-/**
- * Gets the os level keychain impl.
- * @param platform The os platform.
- * @ignore
- */
-export declare const retrieveKeychain: (platform: string) => Promise<KeyChain>;
