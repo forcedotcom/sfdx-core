d5a865fc0f9efa5f3d0fa0d22f6b74e5cd7e39b5
Peter Hale <peter.hale@salesforce.com>
2021-02-24 07:24:33 -0700

    chore: remove unecessary eslint directive

--- test/unit/authInfoTest.ts
+++ test/unit/authInfoTest.ts
@@ -4,7 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 import * as dns from 'dns';
 import * as pathImport from 'path';
 import { cloneJson, env, includes, set } from '@salesforce/kit';
