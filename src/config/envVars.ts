7cdb976a935ea1f9612b6ca44f63c5c8ee935d4c
peternhale <peter.hale@salesforce.com>
2022-02-11 07:25:51 -0700

    chore: move code to a different repo

--- src/config/envVars.ts
+++ src/config/envVars.ts
@@ -429,69 +429,6 @@ export class EnvVars extends Env {
     }, new Map<string, string>());
   }
 
-  public isAutoupdateDisabled(): boolean {
-    return (
-      this.getBoolean(EnvironmentVariable.SFDX_AUTOUPDATE_DISABLE) ||
-      this.getBoolean(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE)
-    );
-  }
-
-  public isAutoupdateDisabledSet(): boolean {
-    return (
-      !!this.getString(EnvironmentVariable.SFDX_AUTOUPDATE_DISABLE) ||
-      !!this.getString(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE)
-    );
-  }
-
-  public setAutoupdateDisabled(value: boolean, updateInstructions?: string): void {
-    this.setBoolean(EnvironmentVariable.SFDX_AUTOUPDATE_DISABLE, value);
-    this.setBoolean(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE, value);
-    if (updateInstructions) {
-      this.setUpdateInstructions(updateInstructions);
-    }
-  }
-
-  public setUpdateInstructions(value: string): void {
-    this.setString(EnvironmentVariable.SFDX_UPDATE_INSTRUCTIONS, value);
-  }
-
-  public isDemoMode(): boolean {
-    return (this.getString(EnvironmentVariable.SFDX_ENV, 'production') || '').toLowerCase() === 'demo';
-  }
-
-  public isInstaller(): boolean {
-    return this.getBoolean(EnvironmentVariable.SFDX_INSTALLER);
-  }
-
-  public getS3HostOverride(): string {
-    return this.getString(EnvironmentVariable.SFDX_S3_HOST) as string;
-  }
-
-  public setS3HostOverride(value: string): void {
-    return this.setString(EnvironmentVariable.SFDX_S3_HOST, value);
-  }
-
-  public getNpmRegistryOverride(): string {
-    return this.getString(EnvironmentVariable.SFDX_NPM_REGISTRY) as string;
-  }
-
-  public setNpmRegistryOverride(value: string): void {
-    return this.setString(EnvironmentVariable.SFDX_NPM_REGISTRY, value);
-  }
-
-  public isLazyRequireEnabled(): boolean {
-    return this.getBoolean(EnvironmentVariable.SFDX_LAZY_LOAD_MODULES);
-  }
-
-  public normalizeAutoupdateDisabled(): void {
-    // Ensure that the legacy envar always causes the oclif counterpart to be set
-    if (this.getBoolean(EnvironmentVariable.SFDX_AUTOUPDATE_DISABLE)) {
-      this.setBoolean(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE, true);
-    } else if (this.getBoolean(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE)) {
-      this.setBoolean(EnvironmentVariable.SFDX_DISABLE_AUTOUPDATE, true);
-    }
-  }
-
   private resolve(): void {
     this.entries().forEach(([key, value]) => {
       if (SUPPORTED_ENV_VARS[key as EnvironmentVariable]) {
