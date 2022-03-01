d4f5976ed1c2bdb56e4a06b184cbdb3f6ad2fdb2
Willie Ruemmele <willieruemmele@gmail.com>
2022-03-01 15:00:26 -0700

    chore: resolve conflicts

--- src/globalInfo/sfdxDataHandler.ts
+++ src/globalInfo/sfdxDataHandler.ts
@@ -201,8 +201,9 @@ export class AliasesHandler extends BaseHandler<SfInfoKeys.ALIASES> {
   public async migrate(): Promise<Pick<SfInfo, SfInfoKeys.ALIASES>> {
     const aliasesFilePath = join(Global.SFDX_DIR, AliasesHandler.SFDX_ALIASES_FILENAME);
     try {
-      const x = await fs.promises.readFile(aliasesFilePath, 'utf8');
-      const sfdxAliases = (parseJson(x) as Record<'orgs', Record<string, string>>).orgs;
+      const sfdxAliases = (
+        parseJson(await fs.promises.readFile(aliasesFilePath, 'utf8')) as Record<'orgs', Record<string, string>>
+      ).orgs;
       return { [this.sfKey]: { ...sfdxAliases } };
     } catch (e) {
       return { [this.sfKey]: {} };
