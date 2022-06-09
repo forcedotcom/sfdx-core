af84374f3f121decb52391918dc80921f44dfdb1
Mike Donnalley <mdonnalley@salesforce.com>
2022-06-09 10:05:32 -0600

    chore: web auth tests

--- test/unit/org/_authInfoTest.ts
+++ test/unit/org/_authInfoTest.ts
@@ -18,7 +18,7 @@ import { Transport } from 'jsforce/lib/transport';
 
 import { OAuth2 } from 'jsforce';
 import { SinonSpy, SinonStub } from 'sinon';
-import { AuthFields, AuthInfo } from '../../../src/org';
+import { AuthFields, AuthInfo, OAuth2Config } from '../../../src/org';
 import { MockTestOrgData, testSetup } from '../../../src/testSetup';
 import { OrgConfigProperties } from '../../../src/org/orgConfigProperties';
 import { OrgAccessor } from '../../../src/stateAggregator';
@@ -26,6 +26,7 @@ import { Crypto } from '../../../src/crypto/crypto';
 
 class AuthInfoMockOrg extends MockTestOrgData {
   public privateKey = 'authInfoTest/jwt/server.key';
+  public expirationDate = '12-02-20';
   public defaultConnectedAppInfo = {
     clientId: 'SalesforceDevelopmentExperience',
     clientSecret: '1384510088588713504',
@@ -80,12 +81,20 @@ describe.only('AuthInfo', () => {
     });
   };
 
-  const stubUserRequest = (): SinonStub => {
+  const stubUserRequest = (
+    userInfoResponse: JsonMap = {
+      statusCode: 200,
+      body: { preferred_username: testOrg.username, organization_id: testOrg.orgId },
+    },
+    userResponse: JsonMap = { statusCode: 200, body: { Username: testOrg.username.toUpperCase() } }
+  ): SinonStub => {
     const userInfoResponseBody = {
-      body: JSON.stringify({ preferred_username: testOrg.username, organization_id: testOrg.orgId }),
+      statusCode: userInfoResponse.statusCode,
+      body: JSON.stringify(userInfoResponse.body),
     };
     const userResponseBody = {
-      body: JSON.stringify({ Username: testOrg.username.toUpperCase() }),
+      statusCode: userResponse.statusCode,
+      body: JSON.stringify(userResponse.body),
     };
     return stubMethod($$.SANDBOX, Transport.prototype, 'httpRequest')
       .onFirstCall()
@@ -181,7 +190,32 @@ describe.only('AuthInfo', () => {
     });
   });
 
-  describe('create()', () => {
+  describe('create', () => {
+    const verifyAuthInfoRefreshToken = (
+      authInfo: AuthInfo,
+      authResponse: { access_token: string; instance_url: string },
+      config?: { clientId?: string }
+    ) => {
+      // Verify the returned AuthInfo instance
+      const authInfoConnOpts = authInfo.getConnectionOptions();
+      expect(authInfoConnOpts).to.have.property('accessToken', authResponse.access_token);
+      expect(authInfoConnOpts).to.have.property('instanceUrl', authResponse.instance_url);
+      expect(authInfoConnOpts).to.not.have.property('refreshToken');
+      expect(authInfoConnOpts['oauth2']).to.have.property('loginUrl', testOrg.instanceUrl);
+      if (config?.clientId) {
+        expect(authInfoConnOpts['oauth2']).to.have.property('clientId', config?.clientId);
+      } else {
+        expect(authInfoConnOpts['oauth2']).to.have.property('clientId', testOrg.defaultConnectedAppInfo.clientId);
+      }
+
+      expect(authInfoConnOpts['oauth2']).to.have.property('redirectUri', testOrg.redirectUri);
+      expect(authInfo.getUsername()).to.equal(testOrg.username);
+      expect(authInfo.isAccessTokenFlow(), 'authInfo.isAccessTokenFlow() should be false').to.be.false;
+      expect(authInfo.isRefreshTokenFlow(), 'authInfo.isRefreshTokenFlow() should be true').to.be.true;
+      expect(authInfo.isJwt(), 'authInfo.isJwt() should be false').to.be.false;
+      expect(authInfo.isOauth(), 'authInfo.isOauth() should be true').to.be.true;
+    };
+
     it('should return an AuthInfo instance when passed an access token as username', async () => {
       $$.stubConfig({ [OrgConfigProperties.ORG_INSTANCE_URL]: testOrg.instanceUrl });
 
@@ -425,31 +459,6 @@ describe.only('AuthInfo', () => {
     });
 
     describe('Refresh Token', () => {
-      const verifyAuthInfo = (
-        authInfo: AuthInfo,
-        authResponse: { access_token: string; instance_url: string },
-        config?: { clientId?: string }
-      ) => {
-        // Verify the returned AuthInfo instance
-        const authInfoConnOpts = authInfo.getConnectionOptions();
-        expect(authInfoConnOpts).to.have.property('accessToken', authResponse.access_token);
-        expect(authInfoConnOpts).to.have.property('instanceUrl', authResponse.instance_url);
-        expect(authInfoConnOpts).to.not.have.property('refreshToken');
-        expect(authInfoConnOpts['oauth2']).to.have.property('loginUrl', testOrg.instanceUrl);
-        if (config?.clientId) {
-          expect(authInfoConnOpts['oauth2']).to.have.property('clientId', config?.clientId);
-        } else {
-          expect(authInfoConnOpts['oauth2']).to.have.property('clientId', testOrg.defaultConnectedAppInfo.clientId);
-        }
-
-        expect(authInfoConnOpts['oauth2']).to.have.property('redirectUri', testOrg.redirectUri);
-        expect(authInfo.getUsername()).to.equal(testOrg.username);
-        expect(authInfo.isAccessTokenFlow(), 'authInfo.isAccessTokenFlow() should be false').to.be.false;
-        expect(authInfo.isRefreshTokenFlow(), 'authInfo.isRefreshTokenFlow() should be true').to.be.true;
-        expect(authInfo.isJwt(), 'authInfo.isJwt() should be false').to.be.false;
-        expect(authInfo.isOauth(), 'authInfo.isOauth() should be true').to.be.true;
-      };
-
       it('should return a refresh token AuthInfo instance when passed a username and refresh token auth options', async () => {
         const refreshTokenConfig = {
           refreshToken: testOrg.refreshToken,
@@ -471,7 +480,7 @@ describe.only('AuthInfo', () => {
           oauth2Options: refreshTokenConfig,
         });
 
-        verifyAuthInfo(authInfo, authResponse);
+        verifyAuthInfoRefreshToken(authInfo, authResponse);
 
         // Verify authInfo.fields are encrypted
         const crypto = await Crypto.create();
@@ -523,7 +532,7 @@ describe.only('AuthInfo', () => {
           oauth2Options: refreshTokenConfig,
         });
 
-        verifyAuthInfo(authInfo, authResponse);
+        verifyAuthInfoRefreshToken(authInfo, authResponse);
 
         // Verify authInfo.fields are encrypted
         const crypto = await Crypto.create();
@@ -577,7 +586,7 @@ describe.only('AuthInfo', () => {
           oauth2Options: refreshTokenConfig,
         });
 
-        verifyAuthInfo(authInfo, authResponse, refreshTokenConfig);
+        verifyAuthInfoRefreshToken(authInfo, authResponse, refreshTokenConfig);
 
         // Verify authInfo.fields are encrypted
         const crypto = await Crypto.create();
@@ -626,6 +635,275 @@ describe.only('AuthInfo', () => {
           expect(err.name).to.equal('RefreshTokenAuthError');
         }
       });
+    });
+
+    describe('Web Auth', () => {
+      it('should return a refresh token AuthInfo instance when passed an authcode', async () => {
+        const authCodeConfig = {
+          authCode: testOrg.authcode,
+          loginUrl: testOrg.loginUrl,
+        };
+        const authCodeConfigClone = cloneJson(authCodeConfig);
+        const authResponse = {
+          access_token: testOrg.accessToken,
+          instance_url: testOrg.instanceUrl,
+          id: '00DAuthInfoTest_orgId/005AuthInfoTest_userId',
+          refresh_token: testOrg.refreshToken,
+        };
+
+        // Stub the http requests (OAuth2.requestToken() and the request for the username)
+        postParamsStub.returns(Promise.resolve(authResponse));
+        const stub = stubUserRequest();
+
+        // Create the refresh token AuthInfo instance
+        const authInfo = await AuthInfo.create({ oauth2Options: authCodeConfig });
+
+        // Ensure we query for the username
+        expect(stub.called).to.be.true;
+        const authInfoConnOpts = authInfo.getConnectionOptions();
+        expect(authInfoConnOpts).to.have.property('accessToken', authResponse.access_token);
+        expect(authInfoConnOpts).to.have.property('instanceUrl', authResponse.instance_url);
+        expect(authInfoConnOpts).to.not.have.property('refreshToken');
+        expect(authInfoConnOpts['oauth2']).to.have.property('loginUrl', testOrg.instanceUrl); // why is this instanceUrl?
+        expect(authInfoConnOpts['oauth2']).to.have.property('clientId', testOrg.defaultConnectedAppInfo.clientId);
+        expect(authInfoConnOpts['oauth2']).to.have.property('redirectUri', testOrg.redirectUri);
+        expect(authInfo.getUsername()).to.equal(testOrg.username.toUpperCase());
+        expect(authInfo.isAccessTokenFlow(), 'authInfo.isAccessTokenFlow() should be false').to.be.false;
+        expect(authInfo.isRefreshTokenFlow(), 'authInfo.isRefreshTokenFlow() should be true').to.be.true;
+        expect(authInfo.isJwt(), 'authInfo.isJwt() should be false').to.be.false;
+        expect(authInfo.isOauth(), 'authInfo.isOauth() should be true').to.be.true;
+
+        // Verify authInfo.fields are encrypted
+        const crypto = await Crypto.create();
+        expect(crypto.decrypt(authInfo.getFields().accessToken)).equals(authResponse.access_token);
+        expect(crypto.decrypt(authInfo.getFields().refreshToken)).equals(authResponse.refresh_token);
+
+        // Verify expected methods are called with expected args
+        expect(authInfoStubs.initAuthOptions.called).to.be.true;
+        expect(authInfoStubs.initAuthOptions.firstCall.args[0]).to.equal(authCodeConfig);
+        expect(authInfoStubs.update.called).to.be.true;
+        expect(authInfoStubs.exchangeToken.called).to.be.true;
+        expect(authInfoStubs.exchangeToken.firstCall.args[0]).to.include(authCodeConfig);
+
+        // Verify the authCodeConfig object was not mutated by init() or buildWebAuthConfig()
+        expect(authCodeConfig).to.deep.equal(authCodeConfigClone);
+
+        const expectedAuthConfig = {
+          accessToken: authResponse.access_token,
+          instanceUrl: testOrg.instanceUrl,
+          username: testOrg.username.toUpperCase(),
+          orgId: authResponse.id.split('/')[0],
+          loginUrl: authCodeConfig.loginUrl,
+          refreshToken: authResponse.refresh_token,
+          isDevHub: false,
+          // These need to be passed in by the consumer. Since they are not, they will show up as undefined.
+          // In a non-test environment, the exchange will fail because no clientId is supplied.
+          clientId: undefined,
+          clientSecret: undefined,
+        };
+        expect(authInfoStubs.update.firstCall.args[0]).to.deep.equal(expectedAuthConfig);
+      });
+
+      it('should return access token and refresh token when using authCode; verifier should be the same.', async () => {
+        /**
+         * The way web oauth works is first you must request a one-time auth code. Typically
+         * you send a hashed number to the server; the number is called the code verifier and the hashed value the code_challenge
+         * After the code is returned you then request an access token by passing along the returned code
+         * an you also include the UNHASHED code verifier value. If these two items match from when the authcode was issued the
+         * access/refresh tokens are then returned.
+         *
+         * Typically the authCode is obtained by authenticating to salesforce via a generated url that contains the
+         * connected app info plus this code challenge. After successful authentication the browser is sent a redirect url
+         * that includes the authCode.
+         *
+         * This test just makes sure the auth code exchange method is using the same codeVerifier. By creating the
+         * codeVerifier instance you can first generate the url then pass it to AuthInfo. Then everything lines up.
+         */
+        const options: OAuth2Config & { authCode?: string } = {
+          clientId: testOrg.clientId,
+          clientSecret: testOrg.clientSecret,
+          loginUrl: testOrg.loginUrl,
+          redirectUri: testOrg.redirectUri,
+        };
+        const oauth2 = new OAuth2(options);
+        options.authCode = '123456';
+
+        const authResponse = {
+          access_token: testOrg.accessToken,
+          instance_url: testOrg.instanceUrl,
+          id: '00DAuthInfoTest_orgId/005AuthInfoTest_userId',
+          refresh_token: testOrg.refreshToken,
+        };
+        // Stub the http requests (OAuth2.requestToken() and the request for the username)
+        postParamsStub.returns(Promise.resolve(authResponse));
+        stubUserRequest();
+
+        await AuthInfo.create({ oauth2Options: options, oauth2 });
+        expect(authInfoStubs.exchangeToken.args.length).to.equal(1);
+        expect(authInfoStubs.exchangeToken.args[0].length).to.equal(2);
+        expect(authInfoStubs.exchangeToken.args[0][1]).to.have.property('codeVerifier', oauth2.codeVerifier);
+      });
+
+      it('should throw a AuthCodeExchangeError when auth fails via an auth code', async () => {
+        const authCodeConfig = {
+          authCode: testOrg.authcode,
+          loginUrl: testOrg.loginUrl,
+        };
+
+        // Stub the http request (OAuth2.requestToken())
+        postParamsStub.throws(new Error('authInfoTest_ERROR_MSG'));
+
+        // Create the auth code AuthInfo instance
+        try {
+          await AuthInfo.create({ oauth2Options: authCodeConfig });
+          assert.fail('should have thrown an error within AuthInfo.buildWebAuthConfig()');
+        } catch (err) {
+          expect(err.name).to.equal('AuthCodeExchangeError');
+        }
+      });
+
+      it('should throw a AuthCodeUsernameRetrievalError when userInfo retrieval fails after auth code exchange', async () => {
+        const authCodeConfig = {
+          authCode: testOrg.authcode,
+          loginUrl: testOrg.loginUrl,
+        };
+        const authResponse = {
+          access_token: testOrg.accessToken,
+          instance_url: testOrg.instanceUrl,
+          id: '00DAuthInfoTest_orgId/005AuthInfoTest_userId',
+          refresh_token: testOrg.refreshToken,
+        };
+
+        // Stub the http request (OAuth2.requestToken())
+        postParamsStub.returns(Promise.resolve(authResponse));
+        stubUserRequest({
+          statusCode: 404,
+          body: [
+            {
+              message: 'Could not retrieve the username after successful auth code exchange.\nDue to: %s',
+              errorCode: 'AuthCodeUsernameRetrievalError',
+            },
+          ],
+        });
+
+        // Create the auth code AuthInfo instance
+        try {
+          await AuthInfo.create({ oauth2Options: authCodeConfig });
+          assert.fail('should have thrown an error within AuthInfo.buildWebAuthConfig()');
+        } catch (err) {
+          expect(err.name).to.equal('AuthCodeUsernameRetrievalError');
+        }
+      });
+
+      it('should throw a AuthCodeUsernameRetrievalError when user sobject retrieval fails after auth code exchange', async () => {
+        const authCodeConfig = {
+          authCode: testOrg.authcode,
+          loginUrl: testOrg.loginUrl,
+        };
+        const authResponse = {
+          access_token: testOrg.accessToken,
+          instance_url: testOrg.instanceUrl,
+          id: '00DAuthInfoTest_orgId/005AuthInfoTest_userId',
+          refresh_token: testOrg.refreshToken,
+        };
+
+        // Stub the http request (OAuth2.requestToken())
+        postParamsStub.returns(Promise.resolve(authResponse));
+
+        stubUserRequest(
+          { body: { preferred_username: testOrg.username, organization_id: testOrg.orgId }, statusCode: 200 },
+          {
+            statusCode: 404,
+            body: [
+              {
+                message: 'Could not retrieve the username after successful auth code exchange.\nDue to: %s',
+                errorCode: 'AuthCodeUsernameRetrievalError',
+              },
+            ],
+          }
+        );
+
+        // Create the auth code AuthInfo instance
+        try {
+          await AuthInfo.create({ oauth2Options: authCodeConfig });
+          assert.fail('should have thrown an error within AuthInfo.buildWebAuthConfig()');
+        } catch (err) {
+          expect(err.name).to.equal('AuthCodeUsernameRetrievalError');
+        }
+      });
+
+      it('should throw an error when neither username nor options have been passed', async () => {
+        try {
+          await AuthInfo.create();
+          assert.fail('Expected AuthInfo.create() to throw an error when no params are passed');
+        } catch (err) {
+          expect(err.name).to.equal('AuthInfoCreationError');
+        }
+      });
+    });
+  });
+
+  describe('save', () => {
+    it('should update the AuthInfo fields, and write to file', async () => {
+      const refreshTokenConfig = {
+        refreshToken: testOrg.refreshToken,
+        loginUrl: testOrg.loginUrl,
+      };
+      const authResponse = {
+        access_token: testOrg.accessToken,
+        instance_url: testOrg.instanceUrl,
+        id: '00DAuthInfoTest_orgId/005AuthInfoTest_userId',
+        expirationDate: testOrg.expirationDate,
+      };
+
+      // Stub the http request (OAuth2.refreshToken())
+      postParamsStub.returns(Promise.resolve(authResponse));
+
+      // Create the AuthInfo instance
+      const authInfo = await AuthInfo.create({
+        username: testOrg.username,
+        oauth2Options: refreshTokenConfig,
+      });
+
+      expect(authInfo.getUsername()).to.equal(testOrg.username);
+
+      // reset the AuthInfo.update stub so we only look at what happens with AuthInfo.save().
+      authInfoStubs.update.resetHistory();
+
+      // Save new fields
+      const changedData = { accessToken: testOrg.accessToken, expirationDate: testOrg.expirationDate };
+
+      // stubMethod($$.SANDBOX, testOrg, 'fetchConfigInfo').returns(Promise.resolve({}));
+      await authInfo.save(changedData);
+
+      expect(authInfoStubs.update.called).to.be.true;
+      expect(authInfoStubs.update.firstCall.args[0]).to.deep.equal(changedData);
+      // expect(configFileWrite.called).to.be.true;
+
+      const crypto = await Crypto.create();
+      // const decryptedActualFields = configFileWrite.lastCall.thisValue.toObject();
+      const decryptedActualFields = $$.stubs.configWrite.lastCall.thisValue.toObject();
+      decryptedActualFields.accessToken = crypto.decrypt(decryptedActualFields.accessToken);
+      decryptedActualFields.refreshToken = crypto.decrypt(decryptedActualFields.refreshToken);
+      decryptedActualFields.clientSecret = crypto.decrypt(decryptedActualFields.clientSecret);
+      delete decryptedActualFields.timestamp;
+      const expectedFields = {
+        accessToken: changedData.accessToken,
+        instanceUrl: testOrg.instanceUrl,
+        username: testOrg.username,
+        orgId: authResponse.id.split('/')[0],
+        loginUrl: refreshTokenConfig.loginUrl,
+        refreshToken: refreshTokenConfig.refreshToken,
+        isDevHub: false,
+        // clientId and clientSecret are now stored in the file, even the defaults.
+        // We just hard code the legacy values here to ensure old auth files will still work.
+        clientId: 'SalesforceDevelopmentExperience',
+        clientSecret: '1384510088588713504',
+        expirationDate: testOrg.expirationDate,
+      };
+      // Note that this also verifies the clientId and clientSecret are not persisted,
+      // and that data is encrypted when saved (because we have to decrypt it to verify here).
+      expect(decryptedActualFields).to.deep.equal(expectedFields);
     });
   });
 });
