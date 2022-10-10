608b11971d893f7417fcfe5f4eea710f38b8e7cf
peternhale <peternhale@users.noreply.github.com>
2022-10-10 12:13:34 -0600

    fix: add tooling query support to autoFetchQuery (#665)
    * fix: add tooling query support to authFectionQuery
    
    @W-11786892@
    
    * chore: apply review change requests

--- src/org/connection.ts
+++ src/org/connection.ts
@@ -352,19 +352,20 @@ export class Connection<S extends Schema = Schema> extends JSForceConnection<S> 
    */
   public async autoFetchQuery<T extends Schema = S>(
     soql: string,
-    queryOptions: Partial<QueryOptions> = {}
+    queryOptions: Partial<QueryOptions & { tooling: boolean }> = { tooling: false }
   ): Promise<QueryResult<T>> {
     const config: ConfigAggregator = await ConfigAggregator.create();
     // take the limit from the calling function, then the config, then default 10,000
     const maxFetch: number =
       ((config.getInfo(OrgConfigProperties.ORG_MAX_QUERY_LIMIT).value as number) || queryOptions.maxFetch) ?? 10000;
 
-    const options: Partial<QueryOptions> = Object.assign(queryOptions, {
+    const { tooling, ...queryOptionsWithoutTooling } = queryOptions;
+
+    const options: Partial<QueryOptions> = Object.assign(queryOptionsWithoutTooling, {
       autoFetch: true,
       maxFetch,
     });
-
-    const query = await this.query<T>(soql, options);
+    const query = tooling ? await this.tooling.query<T>(soql, options) : await this.query<T>(soql, options);
 
     if (query.records.length && query.totalSize > query.records.length) {
       void Lifecycle.getInstance().emitWarning(
