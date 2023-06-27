ab8f7048dc5c09b88e2f355d375e616cbce04d0d
github-action-benchmark <github@users.noreply.github.com>
2023-06-27 23:39:19 +0000

    add Benchmark (benchmarkjs) benchmark result for b04ec4177a139e5ed7409bca0b6c0fc06cbc877d

---
+++ perf-Linux/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1687909159734,
+  "repoUrl": "https://github.com/forcedotcom/sfdx-core",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "shane.mclaughlin@salesforce.com",
+            "name": "mshanemc",
+            "username": "mshanemc"
+          },
+          "committer": {
+            "email": "shane.mclaughlin@salesforce.com",
+            "name": "mshanemc",
+            "username": "mshanemc"
+          },
+          "distinct": true,
+          "id": "b04ec4177a139e5ed7409bca0b6c0fc06cbc877d",
+          "message": "test: install deps",
+          "timestamp": "2023-06-27T18:38:14-05:00",
+          "tree_id": "c0d66a3a7a2ba0516a19c8a819c5efc952aac506",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/b04ec4177a139e5ed7409bca0b6c0fc06cbc877d"
+        },
+        "date": 1687909152702,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "RegExp#test",
+            "value": 35593298,
+            "range": "±1.00%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "String#indexOf",
+            "value": 718378585,
+            "range": "±1.49%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
