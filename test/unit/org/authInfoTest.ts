cfab6a0f045105e7e852d602bf81002574797716
Cristian Dominguez <cdominguez@salesforce.com>
2023-06-29 19:18:54 -0300

    chore: bring back jsforce changes

--- test/unit/org/authInfoTest.ts
+++ test/unit/org/authInfoTest.ts
@@ -18,8 +18,9 @@ import { AnyJson, getJsonMap, JsonMap, toJsonMap } from '@salesforce/ts-types';
 import { expect } from 'chai';
 import { Transport } from 'jsforce/lib/transport';
 
-import { JwtOAuth2Config, OAuth2 } from 'jsforce';
+import { OAuth2 } from 'jsforce';
 import { SinonSpy, SinonStub } from 'sinon';
+import { JwtOAuth2Config } from '../../../src/org/authInfo';
 import { AuthFields, AuthInfo } from '../../../src/org';
 import { MockTestOrgData, shouldThrow, shouldThrowSync, TestContext } from '../../../src/testSetup';
 import { OrgConfigProperties } from '../../../src/org/orgConfigProperties';
