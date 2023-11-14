ac78de4f76f133b7cbe14f3f5b55169e73f97033
mshanemc <shane.mclaughlin@salesforce.com>
2023-11-14 11:03:26 -0600

    refactor: revert #980 in favor of a jsforce solution

--- src/org/org.ts
+++ src/org/org.ts
@@ -698,17 +698,9 @@ export class Org extends AsyncOptionalCreatable<Org.Options> {
       url: this.getConnection().baseUrl(),
       method: 'GET',
     };
+
     const conn = this.getConnection();
-    try {
-      await conn.request(requestInfo);
-    } catch (e) {
-      // an html error page like https://computing-connect-6970-dev-ed.scratch.my.salesforce.com/services/data/v50.0
-      // where the message is an entire html page
-      if (e instanceof Error && (e.name.includes('ERROR_HTTP') || e.message.includes('<html '))) {
-        throw messages.createError('UnexpectedResponse', [], [conn.baseUrl()], e);
-      }
-      throw e;
-    }
+    await conn.request(requestInfo);
   }
 
   /**
