3fea6d7537375ea4c901beac0b7521b47b6c0c1e
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-10-01 17:36:35 -0700

    ci: add check for dependencies that need externalizing during bundle

---
+++ scripts/bundleWithCheck.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+
+const commandToRun = 'yarn build && node ./scripts/build.js';
+
+// Run the command
+exec(commandToRun, (error, stdout, stderr) => {
+  // Combine stdout and stderr to check the entire output
+  const output = `${stdout}\n${stderr}`;
+  console.log('aaaa', output);
+  // Check if the output contains the error string of esbuild
+  if (output.includes('[require-resolve-not-external]')) {
+    console.error('Error: A dependency that has to be externalized in esbuild process is found. Please resolve it!');
+    process.exit(1); // Exit with an error code
+  } else {
+    process.exit(0); // Exit with success code
+  }
+});
