a2b0b36655ba183411b0f2af160e44e77c2a71ae
github-action-benchmark <github@users.noreply.github.com>
2024-10-12 21:12:44 +0000

    add Logger Benchmarks - windows-latest (benchmarkjs) benchmark result for 09a9eefa587b875996f9bdcac356004ecf25cdeb

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728165200117,
+  "lastUpdate": 1728767563779,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - windows-latest": [
@@ -24348,6 +24348,72 @@ window.BENCHMARK_DATA = {
             "range": "±5.59%",
             "unit": "ops/sec",
             "extra": "65 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09a9eefa587b875996f9bdcac356004ecf25cdeb",
+          "message": "fix(deps): bump @jsforce/jsforce-node from 3.4.1 to 3.5.2\n\nBumps [@jsforce/jsforce-node](https://github.com/jsforce/jsforce) from 3.4.1 to 3.5.2.\n- [Release notes](https://github.com/jsforce/jsforce/releases)\n- [Changelog](https://github.com/jsforce/jsforce/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/jsforce/jsforce/compare/3.4.1...3.5.2)\n\n---\nupdated-dependencies:\n- dependency-name: \"@jsforce/jsforce-node\"\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-10-12T21:07:58Z",
+          "tree_id": "9658fb989b94d6b4a175a0fd99e1cfb33d135bda",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/09a9eefa587b875996f9bdcac356004ecf25cdeb"
+        },
+        "date": 1728767550736,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 347480,
+            "range": "±0.50%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 854052,
+            "range": "±7.52%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 639143,
+            "range": "±9.99%",
+            "unit": "ops/sec",
+            "extra": "56 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 1923,
+            "range": "±255.75%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 485653,
+            "range": "±7.34%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 322163,
+            "range": "±4.38%",
+            "unit": "ops/sec",
+            "extra": "74 samples"
           }
         ]
       }
