0a1228f98db6448231bb198e3cae534734305d70
github-action-benchmark <github@users.noreply.github.com>
2025-04-26 21:39:33 +0000

    add Logger Benchmarks - windows-latest (benchmarkjs) benchmark result for f2ccbe64e1b1f6a2ce5128b1542e9b9cf66f1ab6

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744404564085,
+  "lastUpdate": 1745703573594,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - windows-latest": [
@@ -26658,6 +26658,72 @@ window.BENCHMARK_DATA = {
             "range": "±8.29%",
             "unit": "ops/sec",
             "extra": "63 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2ccbe64e1b1f6a2ce5128b1542e9b9cf66f1ab6",
+          "message": "chore(dev-deps): bump esbuild from 0.23.1 to 0.25.3\n\nBumps [esbuild](https://github.com/evanw/esbuild) from 0.23.1 to 0.25.3.\n- [Release notes](https://github.com/evanw/esbuild/releases)\n- [Changelog](https://github.com/evanw/esbuild/blob/main/CHANGELOG-2024.md)\n- [Commits](https://github.com/evanw/esbuild/compare/v0.23.1...v0.25.3)\n\n---\nupdated-dependencies:\n- dependency-name: esbuild\n  dependency-version: 0.25.3\n  dependency-type: direct:development\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-04-26T21:33:28Z",
+          "tree_id": "3d31ccb2d9fda2e2b9cc0ec321867ed635080bf1",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/f2ccbe64e1b1f6a2ce5128b1542e9b9cf66f1ab6"
+        },
+        "date": 1745703555620,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 332392,
+            "range": "±0.89%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 842768,
+            "range": "±9.14%",
+            "unit": "ops/sec",
+            "extra": "53 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 38867,
+            "range": "±184.34%",
+            "unit": "ops/sec",
+            "extra": "71 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 439871,
+            "range": "±8.87%",
+            "unit": "ops/sec",
+            "extra": "75 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 541208,
+            "range": "±8.88%",
+            "unit": "ops/sec",
+            "extra": "59 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 17144,
+            "range": "±188.31%",
+            "unit": "ops/sec",
+            "extra": "34 samples"
           }
         ]
       }
