34f156e049f2fdea25a61f586f26ae54f6c43719
mshanemc <shane.mclaughlin@salesforce.com>
2025-05-30 13:48:23 -0500

    chore: pr feedback

--- src/util/time.ts
+++ src/util/time.ts
@@ -5,4 +5,5 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 /** using globalThis.performance instead importing from node:perf_hooks so it works in browser */
-export const nowBigInt = (): bigint => BigInt((globalThis.performance.now() + performance.timeOrigin) * 1_000_000);
+export const nowBigInt = (): bigint =>
+  BigInt((globalThis.performance.now() + globalThis.performance.timeOrigin) * 1_000_000);
