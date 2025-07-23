bfe1d846f4af705215601c3340204a58f0742699
github-action-benchmark <github@users.noreply.github.com>
2025-07-23 13:15:12 +0000

    add Logger Benchmarks - windows-latest (benchmarkjs) benchmark result for e0c833d8a7fc7ce9e98d0889e6d186eedea21cb0

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753139765561,
+  "lastUpdate": 1753276512258,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - windows-latest": [
@@ -30750,6 +30750,72 @@ window.BENCHMARK_DATA = {
             "range": "±187.25%",
             "unit": "ops/sec",
             "extra": "47 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cdominguez@salesforce.com",
+            "name": "Cristian Dominguez",
+            "username": "cristiand391"
+          },
+          "committer": {
+            "email": "cdominguez@salesforce.com",
+            "name": "Cristian Dominguez",
+            "username": "cristiand391"
+          },
+          "distinct": true,
+          "id": "e0c833d8a7fc7ce9e98d0889e6d186eedea21cb0",
+          "message": "chore: add unit tests for getMetadataUI method",
+          "timestamp": "2025-07-23T10:08:57-03:00",
+          "tree_id": "1d217286fc803e768c0266af65b86c0f1780b031",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/e0c833d8a7fc7ce9e98d0889e6d186eedea21cb0"
+        },
+        "date": 1753276498598,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 355062,
+            "range": "±0.67%",
+            "unit": "ops/sec",
+            "extra": "97 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 929564,
+            "range": "±8.27%",
+            "unit": "ops/sec",
+            "extra": "72 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 38779,
+            "range": "±185.60%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 429238,
+            "range": "±10.15%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 596661,
+            "range": "±7.93%",
+            "unit": "ops/sec",
+            "extra": "65 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 13655,
+            "range": "±189.96%",
+            "unit": "ops/sec",
+            "extra": "32 samples"
           }
         ]
       }
