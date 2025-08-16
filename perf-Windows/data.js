67c040c76c5dd80dea39a315880c6f642a41b4d6
github-action-benchmark <github@users.noreply.github.com>
2025-08-16 21:53:57 +0000

    add Logger Benchmarks - windows-latest (benchmarkjs) benchmark result for b7bbaceb6b7fb876bc301e7e9e893132bbfaa557

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754645034022,
+  "lastUpdate": 1755381236766,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - windows-latest": [
@@ -31344,6 +31344,72 @@ window.BENCHMARK_DATA = {
             "range": "±185.83%",
             "unit": "ops/sec",
             "extra": "43 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7bbaceb6b7fb876bc301e7e9e893132bbfaa557",
+          "message": "chore(dev-deps): bump esbuild-plugin-pino from 2.2.0 to 2.3.1\n\nBumps [esbuild-plugin-pino](https://github.com/davipon/esbuild-plugin-pino) from 2.2.0 to 2.3.1.\n- [Release notes](https://github.com/davipon/esbuild-plugin-pino/releases)\n- [Changelog](https://github.com/wd-David/esbuild-plugin-pino/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/davipon/esbuild-plugin-pino/compare/v2.2.0...v2.3.1)\n\n---\nupdated-dependencies:\n- dependency-name: esbuild-plugin-pino\n  dependency-version: 2.3.1\n  dependency-type: direct:development\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-08-16T21:48:39Z",
+          "tree_id": "0e58b3a09c7fba946ef0064fc1ffc17411b46598",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/b7bbaceb6b7fb876bc301e7e9e893132bbfaa557"
+        },
+        "date": 1755381223862,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 352022,
+            "range": "±0.21%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 981421,
+            "range": "±7.68%",
+            "unit": "ops/sec",
+            "extra": "57 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 32631,
+            "range": "±187.54%",
+            "unit": "ops/sec",
+            "extra": "52 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 192114,
+            "range": "±108.83%",
+            "unit": "ops/sec",
+            "extra": "76 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 541170,
+            "range": "±7.86%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 17931,
+            "range": "±187.71%",
+            "unit": "ops/sec",
+            "extra": "36 samples"
           }
         ]
       }
