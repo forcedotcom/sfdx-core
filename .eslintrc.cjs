7a7f7088dc3377100f6509b3b05f800094106943
Eric Willhoit <ewillhoit@salesforce.com>
2025-08-29 13:09:15 -0500

    fix: oss compliance

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
-
 // Generated - Do not modify. Controlled by @salesforce/dev-scripts
 // See more at https://github.com/forcedotcom/sfdx-dev-packages/tree/master/packages/dev-scripts
 
