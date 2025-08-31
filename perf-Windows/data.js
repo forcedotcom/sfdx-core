ff0403914776ef166b16a829f2182a582e4572dd
github-action-benchmark <github@users.noreply.github.com>
2025-08-31 03:13:20 +0000

    add Logger Benchmarks - windows-latest (benchmarkjs) benchmark result for 75f111eac74c0d1e9e9f2784152c4353aa3202bd

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756588020061,
+  "lastUpdate": 1756610000536,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - windows-latest": [
@@ -32664,6 +32664,72 @@ window.BENCHMARK_DATA = {
             "range": "±187.23%",
             "unit": "ops/sec",
             "extra": "37 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "75f111eac74c0d1e9e9f2784152c4353aa3202bd",
+          "message": "chore(dev-deps): bump @types/jsonwebtoken from 9.0.9 to 9.0.10\n\nBumps [@types/jsonwebtoken](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/jsonwebtoken) from 9.0.9 to 9.0.10.\n- [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)\n- [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/jsonwebtoken)\n\n---\nupdated-dependencies:\n- dependency-name: \"@types/jsonwebtoken\"\n  dependency-version: 9.0.10\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-08-31T03:07:04Z",
+          "tree_id": "8a65114fe859fd9e6f07d4d45e20e7d7aca97385",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/75f111eac74c0d1e9e9f2784152c4353aa3202bd"
+        },
+        "date": 1756609987155,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 325077,
+            "range": "±1.53%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 903315,
+            "range": "±6.68%",
+            "unit": "ops/sec",
+            "extra": "60 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 640604,
+            "range": "±8.54%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 19226,
+            "range": "±187.02%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 566960,
+            "range": "±6.76%",
+            "unit": "ops/sec",
+            "extra": "61 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 17746,
+            "range": "±188.08%",
+            "unit": "ops/sec",
+            "extra": "35 samples"
           }
         ]
       }
