6c26d238f8abc871586a0b049d0645d5f1d3e32b
mshanemc <shane.mclaughlin@salesforce.com>
2025-12-01 17:38:21 -0600

    chore: still prettier noise

--- scripts/schemas/build-schema-project.ts
+++ scripts/schemas/build-schema-project.ts
@@ -1,6 +1,4 @@
 import { writeFileSync } from 'node:fs';
-// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-const prettier = require('prettier');
 import { z } from 'zod';
 import { ProjectJsonSchema } from '../../src/schema/sfdx-project/sfdxProjectJson';
 import { schemaSchema, target } from './consts';
@@ -9,25 +7,21 @@ import { schemaSchema, target } from './consts';
  * Build script for sfdx-project.schema.json using Zod's native JSON Schema generation
  */
 
-const run = async (): Promise<void> => {
-  console.log('generating sfdx-project schema from Zod schema');
+console.log('generating sfdx-project schema from Zod schema');
 
-  const projectSchema = z.toJSONSchema(ProjectJsonSchema, {
-    target,
-    override: (ctx) => {
-      if (ctx.path.length === 0) {
-        ctx.jsonSchema.$schema = schemaSchema;
-        ctx.jsonSchema.$id = 'http://schemas.salesforce.com/sfdx-project.json';
-        ctx.jsonSchema.title = 'Salesforce DX Project File';
-      }
-    },
-  });
-  const projectSchemaJson = await prettier.format(JSON.stringify(projectSchema), { parser: 'json' });
+const projectSchema = z.toJSONSchema(ProjectJsonSchema, {
+  target,
+  override: (ctx) => {
+    if (ctx.path.length === 0) {
+      ctx.jsonSchema.$schema = schemaSchema;
+      ctx.jsonSchema.$id = 'http://schemas.salesforce.com/sfdx-project.json';
+      ctx.jsonSchema.title = 'Salesforce DX Project File';
+    }
+  },
+});
+const projectSchemaJson = JSON.stringify(projectSchema, null, 2);
 
-  console.log('writing sfdx-project schema to src/');
-  writeFileSync('src/schema/sfdx-project/sfdx-project.schema.json', projectSchemaJson);
+console.log('writing sfdx-project schema to src/');
+writeFileSync('src/schema/sfdx-project/sfdx-project.schema.json', projectSchemaJson);
 
-  console.log('done');
-};
-
-void run();
+console.log('done');
--- scripts/schemas/build-schema-scratch.ts
+++ scripts/schemas/build-schema-scratch.ts
@@ -1,6 +1,4 @@
 import { writeFileSync } from 'node:fs';
-// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-const prettier = require('prettier');
 import { z } from 'zod';
 import { ScratchOrgDefSchema } from '../../src/schema/project-scratch-def/scratchOrgDef';
 import { schemaSchema, target } from './consts';
@@ -9,27 +7,23 @@ import { schemaSchema, target } from './consts';
  * Build script for project-scratch-def.schema.json using Zod's native JSON Schema generation
  */
 
-const run = async (): Promise<void> => {
-  console.log('generating project-scratch-def schema from Zod schema');
+console.log('generating project-scratch-def schema from Zod schema');
 
-  const scratchDefSchema = z.toJSONSchema(ScratchOrgDefSchema, {
-    target,
-    override: (ctx) => {
-      if (ctx.path.length === 0) {
-        ctx.jsonSchema.$schema = schemaSchema;
-        ctx.jsonSchema.$id = 'https://schemas.salesforce.com/project-scratch-def.json';
-        ctx.jsonSchema.title = 'Scratch Org Definition Configuration';
-        ctx.jsonSchema.description =
-          'The scratch org definition file contains the configuration values that determine the shape of the scratch org.';
-      }
-    },
-  });
-  const scratchDefSchemaJson = await prettier.format(JSON.stringify(scratchDefSchema), { parser: 'json' });
+const scratchDefSchema = z.toJSONSchema(ScratchOrgDefSchema, {
+  target,
+  override: (ctx) => {
+    if (ctx.path.length === 0) {
+      ctx.jsonSchema.$schema = schemaSchema;
+      ctx.jsonSchema.$id = 'https://schemas.salesforce.com/project-scratch-def.json';
+      ctx.jsonSchema.title = 'Scratch Org Definition Configuration';
+      ctx.jsonSchema.description =
+        'The scratch org definition file contains the configuration values that determine the shape of the scratch org.';
+    }
+  },
+});
+const scratchDefSchemaJson = JSON.stringify(scratchDefSchema, null, 2);
 
-  console.log('writing project-scratch-def schema to src/');
-  writeFileSync('src/schema/project-scratch-def/project-scratch-def.schema.json', scratchDefSchemaJson);
+console.log('writing project-scratch-def schema to src/');
+writeFileSync('src/schema/project-scratch-def/project-scratch-def.schema.json', scratchDefSchemaJson);
 
-  console.log('done');
-};
-
-void run();
+console.log('done');
