3fce87401199376ec2780b46fc3891d85591b0bc
Joshua Feingold <jfeingold35@gmail.com>
2026-01-21 15:44:30 -0600

    feat: jwt detection regex strictened @W-20036916@

--- src/util/sfdc.ts
+++ src/util/sfdc.ts
@@ -55,7 +55,8 @@ export const validatePathDoesNotContainInvalidChars = (value: string): boolean =
   !/[\["\?<>\|\]]+/.test(value);
 
 export const accessTokenRegex = /(00D\w{12,15})![.\w]*/;
-export const jwtTokenRegex = /[A-Za-z0-9+-]+\.[A-Za-z0-9+=-]+\.[A-Za-z0-9+=-]/;
+// 'eyJ' strongly suggests that this is a base64 JSON, and so the general shape of the rest of it is enough to presume it's a JWT.
+export const jwtTokenRegex = /eyJ[A-Za-z0-9+-]+\.[A-Za-z0-9+=-]+\.[A-Za-z0-9+=-]/;
 export const sfdxAuthUrlRegex =
   /force:\/\/([a-zA-Z0-9._-]+):([a-zA-Z0-9._-]*):([a-zA-Z0-9._-]+={0,2})@([a-zA-Z0-9._-]+)/;
 
