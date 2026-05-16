c6333733d70a6743d112e62056aa0396c84e40bc
github-action-benchmark <github@users.noreply.github.com>
2026-05-16 21:56:19 +0000

    add Logger Benchmarks - ubuntu-latest (benchmarkjs) benchmark result for a502227698a8e683ee1c76f61bd5a7d766212cd1

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778877184345,
+  "lastUpdate": 1778968579221,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - ubuntu-latest": [
@@ -39990,6 +39990,72 @@ window.BENCHMARK_DATA = {
             "range": "±206.87%",
             "unit": "ops/sec",
             "extra": "17 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a502227698a8e683ee1c76f61bd5a7d766212cd1",
+          "message": "fix(deps): bump form-data from 4.0.4 to 4.0.5\n\nBumps [form-data](https://github.com/form-data/form-data) from 4.0.4 to 4.0.5.\n- [Release notes](https://github.com/form-data/form-data/releases)\n- [Changelog](https://github.com/form-data/form-data/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/form-data/form-data/compare/v4.0.4...v4.0.5)\n\n---\nupdated-dependencies:\n- dependency-name: form-data\n  dependency-version: 4.0.5\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-05-16T21:52:47Z",
+          "tree_id": "e555700c1f471c374e27b4541d04d2e1369e9aba",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/a502227698a8e683ee1c76f61bd5a7d766212cd1"
+        },
+        "date": 1778968569579,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 486959,
+            "range": "±0.39%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 1116219,
+            "range": "±6.25%",
+            "unit": "ops/sec",
+            "extra": "37 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 26798,
+            "range": "±207.82%",
+            "unit": "ops/sec",
+            "extra": "15 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 592824,
+            "range": "±9.08%",
+            "unit": "ops/sec",
+            "extra": "54 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 482215,
+            "range": "±8.73%",
+            "unit": "ops/sec",
+            "extra": "52 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 9636,
+            "range": "±207.81%",
+            "unit": "ops/sec",
+            "extra": "16 samples"
           }
         ]
       }
