cb8f23702dec27be5535dab4d9deb2f98b8bfeee
Jamie Feingold <jfeingold@salesforce.com>
2026-08-21 08:49:46 -0500

    fix: rejiggered eslint.config.mjs (W-23928423)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,30 @@
-export { default } from 'eslint-config-salesforce-typescript';
+import tsconfigs from 'eslint-config-salesforce-typescript';
+
+const configs = [
+  ...tsconfigs,
+  {
+    files: ['./test/**/*'],
+    rules: {
+      // Allow assert style expressions. i.e. expect(true).to.be.true
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // Tests usually access private or protected methods/variables
+      '@typescript-eslint/ban-ts-comment': 'off',
+
+      // It is common for tests to stub out method.
+      '@typescript-eslint/camelcase': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+
+      // Return types are defined by the source code. Allows for quick overwrites.
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      // Mocked out the methods that shouldn't do anything in the tests.
+      '@typescript-eslint/no-empty-function': 'off',
+      // Easily return a promise in a mocked method.
+      '@typescript-eslint/require-await': 'off',
+    },
+  },
+];
+
+export default configs;
