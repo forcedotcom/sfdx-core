5a3e5ce402494d9931da160f08d32a1502467159
github-action-benchmark <github@users.noreply.github.com>
2026-08-25 17:26:53 +0000

    add Logger Benchmarks - ubuntu-latest (benchmarkjs) benchmark result for a583ed7fdcb5be4b9d1ff439464944f12b9b0615

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787677626757,
+  "lastUpdate": 1787678813397,
   "repoUrl": "https://github.com/forcedotcom/sfdx-core",
   "entries": {
     "Logger Benchmarks - ubuntu-latest": [
@@ -44940,6 +44940,72 @@ window.BENCHMARK_DATA = {
             "range": "±8.01%",
             "unit": "ops/sec",
             "extra": "61 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.hale@salesforce.com",
+            "name": "peternhale",
+            "username": "peternhale"
+          },
+          "committer": {
+            "email": "peter.hale@salesforce.com",
+            "name": "peternhale",
+            "username": "peternhale"
+          },
+          "distinct": true,
+          "id": "a583ed7fdcb5be4b9d1ff439464944f12b9b0615",
+          "message": "feat: add Apex Symbols Connection API (W-23973889)",
+          "timestamp": "2026-08-25T11:21:53-06:00",
+          "tree_id": "34749de430951312803fecbefd278a4b07f68faa",
+          "url": "https://github.com/forcedotcom/sfdx-core/commit/a583ed7fdcb5be4b9d1ff439464944f12b9b0615"
+        },
+        "date": 1787678802689,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Child logger creation",
+            "value": 487740,
+            "range": "±0.42%",
+            "unit": "ops/sec",
+            "extra": "99 samples"
+          },
+          {
+            "name": "Logging a string on root logger",
+            "value": 1133805,
+            "range": "±8.81%",
+            "unit": "ops/sec",
+            "extra": "39 samples"
+          },
+          {
+            "name": "Logging an object on root logger",
+            "value": 32705,
+            "range": "±202.30%",
+            "unit": "ops/sec",
+            "extra": "19 samples"
+          },
+          {
+            "name": "Logging an object with a message on root logger",
+            "value": 594091,
+            "range": "±7.26%",
+            "unit": "ops/sec",
+            "extra": "54 samples"
+          },
+          {
+            "name": "Logging an object with a redacted prop on root logger",
+            "value": 493537,
+            "range": "±9.67%",
+            "unit": "ops/sec",
+            "extra": "53 samples"
+          },
+          {
+            "name": "Logging a nested 3-level object on root logger",
+            "value": 6772,
+            "range": "±216.77%",
+            "unit": "ops/sec",
+            "extra": "12 samples"
           }
         ]
       }
